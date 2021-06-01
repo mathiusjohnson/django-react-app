281bcf17167ed7bc25a639e1fe9f8c5bbaaa6ec5
Mathius <devachievem@gmail.com>
2021-06-01 07:41:26 -0700

    improve error handling for editperson

--- src/components/PersonListItem/EditPerson.js
+++ src/components/PersonListItem/EditPerson.js
@@ -5,6 +5,8 @@ export default function EditPerson ({id, oldName, oldAge, onCancel, onSave, addr
   const [name, setName] = useState(oldName || "");
   const [age, setAge] = useState(oldAge || "");
   const [error, setError] = useState("");
+  const [nameBorder, setNameBorder] = useState("green")
+  const [ageBorder, setAgeBorder] = useState("green")
 
   const reset = () => {
     setName("");
@@ -17,14 +19,18 @@ export default function EditPerson ({id, oldName, oldAge, onCancel, onSave, addr
   };
 
   function validate() {
-    if (name === "") {
+    if (name === undefined) {
+      setNameBorder("red")
       setError("Name cannot be blank");
       return;
     }
-    if (age === null) {
+
+    if (age === undefined) {
+      setAgeBorder("red")
       setError("Age cannot be blank")
       return;
     }
+    
     if (name !== "" && age !== null){
       setError("");
       onSave(name, age)
@@ -36,7 +42,7 @@ export default function EditPerson ({id, oldName, oldAge, onCancel, onSave, addr
       <section className="col-span-4 grid grid-cols-4 ">
         <form className="col-span-2 grid grid-cols-2" autoComplete="off" onSubmit={(event) => event.preventDefault()}>
             <input
-              className="border-2 border-solid border-green-500 rounded text-center mx-2"
+              className={`border-2 border-solid border-${nameBorder}-500 rounded text-center mx-2`}
               name="name"
               type="text"
               placeholder="Enter Name"
@@ -48,7 +54,7 @@ export default function EditPerson ({id, oldName, oldAge, onCancel, onSave, addr
               data-testid=""
             />
           <input
-              className="border-2 border-solid border-green-500 rounded text-center mx-2"
+              className={`border-2 border-solid border-${ageBorder}-500 rounded text-center mx-2`}
               name="name"
               type="number"
               placeholder="Enter age"
