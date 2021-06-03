264c4c984140a8e9c4419a104e020e9fd90e8cec
Mathius <devachievem@gmail.com>
2021-06-03 08:44:43 -0700

    add test for useVisualMode hook

--- src/components/AddressOneInput.js
+++ src/components/AddressOneInput.js
@@ -2,9 +2,68 @@ import React from 'react';
 
 const AddressOneInput = () => {
   return (
-    <div>
-      
-    </div>
+    <div className="flex flex-wrap col-span-1">
+    <input
+      className={`border-2 border-solid border-${addressBorder}-500 rounded text-center mx-2`}
+      name="addressOne"
+      type="text"
+      placeholder="Enter Street"
+      value={addressOneStreet || "" }
+      onChange={(event) => {
+        setAddressOneStreet(event.target.value) 
+        addressBorder = "green"
+        setError("")
+      }}
+    />
+    <input
+      className={`border-2 border-solid border-${addressBorder}-500 rounded text-center mx-2`}
+      name="addressOne"
+      type="text"
+      placeholder="Enter City"
+      value={addressOneCity || "" }
+      onChange={(event) => {
+        setAddressOneCity(event.target.value) 
+        addressBorder = "green"
+        setError("")
+      }}
+    />
+    <input
+      className={`border-2 border-solid border-${addressBorder}-500 rounded text-center mx-2`}
+      name="addressOne"
+      type="text"
+      placeholder="Enter Region"
+      value={addressOneRegion || "" }
+      onChange={(event) => {
+        setAddressOneRegion(event.target.value) 
+        addressBorder = "green"
+        setError("")
+      }}
+    />
+    <input
+      className={`border-2 border-solid border-${addressBorder}-500 rounded text-center mx-2`}
+      name="addressOne"
+      type="text"
+      placeholder="Enter Country"
+      value={addressOneCountry || "" }
+      onChange={(event) => {
+        setAddressOneCountry(event.target.value) 
+        addressBorder = "green"
+        setError("")
+      }}
+    />
+    <input
+      className={`border-2 border-solid border-${addressBorder}-500 rounded text-center mx-2`}
+      name="addressOne"
+      type="text"
+      placeholder="Enter Postal Code"
+      value={addressOnePostal || "" }
+      onChange={(event) => {
+        setAddressOnePostal(event.target.value) 
+        addressBorder = "green"
+        setError("")
+      }}
+    />
+  </div>
   );
 };
 
--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import { People } from './PersonList';
 function App() {
   return (
     <div className="App">
+        <h1>People</h1>
       <Header />
       <People />
     </div>
