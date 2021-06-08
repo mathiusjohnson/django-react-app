7d4a8f4145a886a90a037d61f3fc1329bbae8b32
Mathius <devachievem@gmail.com>
2021-06-08 13:55:44 -0700

    fix showaddress to show nothing if there is no address

--- src/components/HomePage/PersonListItem/ShowAddress.js
+++ src/components/HomePage/PersonListItem/ShowAddress.js
@@ -1,17 +1,23 @@
 import React from 'react';
 
 const ShowAddressOne = ({address}) => {
-  const street = address.street
-  const city = address.city
-  const region = address.region
-  const country = address.country
-  const postalCode = address.postalCode
-  return (
-    <div>
-      <p>{street}, {city}</p>
-      <p>{region}, {country}, {postalCode}</p>
-    </div>
-  );
+
+    console.log(address.street);
+    if (address.street !== null) {
+        const street = address.street
+        const city = address.city
+        const region = address.region
+        const country = address.country
+        const postalCode = address.postalCode
+        return (
+          <div>
+            <p>{street}, {city}</p>
+            <p>{region}, {country}, {postalCode}</p>
+          </div>
+        );
+    } else {
+        return <div></div>;
+    }
 };
 
 export default ShowAddressOne;
